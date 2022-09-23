71c5de9e3262239f45a0c4cc2b79b415f06bcf20
timothynoyan <timothy.noyan@my.uwi.edu>
2022-09-23 23:22:24 +0000

    Q1 hello_world

--- main/hello_world_main.c
+++ main/hello_world_main.c
@@ -15,7 +15,7 @@
 
 void app_main()
 {
-    printf("Hello world!\n");
+    printf("Hello world! My name is Timothy Noyan and my Student ID# is 816024601\n");
 
     /* Print chip information */
     esp_chip_info_t chip_info;
@@ -28,7 +28,7 @@ void app_main()
     printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
             (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
 
-    for (int i = 10; i >= 0; i--) {
+    for (int i = 1; i >= 0; i--) {
         printf("Restarting in %d seconds...\n", i);
         vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
